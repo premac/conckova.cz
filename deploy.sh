2ee4dd326a31d0a9f33b1fa8676a5d00d357e639
Premysl Concka <concka@gmail.com>
2014-05-25 22:38:13 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while [ -n $PASS]; do
+	echo "Password: "
+	read PASS
+done;	
+
+
+upload() 
+{
+	dir=$1
+
+	for file in `ls -F $dir | grep -v '[/]'`; do
+		path=$dir/$file
+		echo "# Uploading $path"
+		curl -T $path ftp://ftp.conckova.cz/www/$dir/ --user conckova.cz:$PASS
+	done		
+
+	for dir in `ls -d $dir/*/ 2> /dev/null`; do
+		upload `echo $dir | sed 's/\/$//g'`
+	done		
+}
+
+upload .
+
+
