cfcf36768584c00d6edac10630585d30051d0bdb
Přemysl Čončka <premysl.concka@vertice.one>
2025-06-15 16:51:49 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="modern-footer">
-  <p>&copy; <?php echo date('Y'); ?> Věra Čončková &middot; vytvořil <a href="mailto:premysl@concka.com">Přemysl Čonka</a></p>
+  <p>&copy; <?php echo date('Y'); ?> Věra Čončková &middot; vytvořil <a href="mailto:premysl@concka.com">Přemysl Čončka</a></p>
 </footer>
 </body>
 </html>
